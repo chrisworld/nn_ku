dd6f2df87dcfdd37a86bf996e9371d32463bba22
chrisworld <christian.walter@outlook.at>
2018-11-22 10:39:45 +0100

    classified Batches one hot

--- task2/ClassifiedBatches.py
+++ task2/ClassifiedBatches.py
@@ -1,7 +1,13 @@
+import tensorflow as tf
 
 class ClassifiedBatches():
-  def __init__(self, examples, classes, batch_size):
+  def __init__(self, examples, classes, batch_size, num_classes=26):
     self.examples = examples
     self.classes = classes
     self.batch_size = batch_size
+
+    c_oh = tf.one_hot(classes-1, num_classes)
+    with tf.Session() as sess:
+      self.classes_one_hot = sess.run(c_oh)
+
 
