7d664ec250f69b4b3c790a0b32d76f2a7085f95e
Christian <christian.walter@outlook.at>
2018-11-24 12:01:25 +0100

    model without hidden layer and print layout improved

--- task2/Model.py
+++ task2/Model.py
@@ -19,8 +19,12 @@ class Model():
       # set connections upon layer
       if layer == 0: 
         print('Input W/b in layer: ', layer)
-        self.W.append(tf.Variable(rd.randn(self.n_in, self.n_hidden) / np.sqrt(self.n_in), trainable=True))
-        self.b.append(tf.Variable(np.zeros(self.n_hidden), trainable=True))
+        if n_layer == 0:
+          self.W.append(tf.Variable(rd.randn(self.n_in, self.n_out) / np.sqrt(self.n_in), trainable=True))
+          self.b.append(tf.Variable(np.zeros(self.n_out), trainable=True))
+        else:
+          self.W.append(tf.Variable(rd.randn(self.n_in, self.n_hidden) / np.sqrt(self.n_in), trainable=True))
+          self.b.append(tf.Variable(np.zeros(self.n_hidden), trainable=True))
       elif layer == n_layer:
         print('Output W/b in layer: ', layer)
         self.W.append(tf.Variable(rd.randn(self.n_hidden, self.n_out) / np.sqrt(self.n_hidden), trainable=True))
@@ -43,7 +47,10 @@ class Model():
         self.h.append(tf.nn.tanh(tf.matmul(self.h[layer-1], self.W[layer]) + self.b[layer]))
 
     # output activation
-    self.z = tf.nn.softmax(tf.matmul(self.h[n_layer-1], self.W[n_layer]) + self.b[n_layer])
+    if n_layer == 0:
+      self.z = tf.nn.softmax(tf.matmul(self.x, self.W[n_layer]) + self.b[n_layer])
+    else:
+      self.z = tf.nn.softmax(tf.matmul(self.h[n_layer-1], self.W[n_layer]) + self.b[n_layer])
 
     # labels
     self.z_ = tf.placeholder(shape=(None, self.n_out),dtype=tf.float64)
