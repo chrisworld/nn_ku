aef9cc1fa7296d32c4ba028be9828a4c9cbfb5ed
chrisworld <christian.walter@outlook.at>
2018-12-04 15:21:09 +0100

    ResNetModel v1

--- task4/debug_cw.py
+++ task4/debug_cw.py
@@ -4,6 +4,7 @@ from ClassifiedBatches import ClassifiedBatches
 from Trainer import Trainer
 from Evaluator import Evaluator
 from Model import Model
+from ResNetModel import ResNetModel
 from ModelTester import ModelTester
 
 from nn18_ex2_load import load_isolet
@@ -26,11 +27,10 @@ if __name__ == '__main__':
   logging.basicConfig(filename=log_file_path + log_file_name, level=logging.INFO)
 
   # Parameters and model
-  epochs = 10
-  learning_rates = [0.01]
-  n_hidden = [150]
-  n_layer = [1]
-
+  epochs = 50
+  learning_rate = 0.01
+  n_hidden = 40
+  n_layer = 9
   batch_size = 20
 
   # Batch Normalizer
@@ -41,15 +41,15 @@ if __name__ == '__main__':
   train_batches.examples_validation = test_batches.examples_validation
   train_batches.classes_validation = test_batches.classes_validation
 
-  model_tester = ModelTester(epochs, learning_rates, n_hidden, n_layer)
-  model_tester.run(train_batches, test_batches)
-
-  # run best model at best epoch with whole training set and use test set as validation
-  #epochs = 10
-  #learning_rates = [0.01]
-  #n_hidden = [150]
-  #n_layer = [1]
-  #train_batches = bn.getBatches(X, C, is_validation=False)
   #model_tester = ModelTester(epochs, learning_rates, n_hidden, n_layer)
   #model_tester.run(train_batches, test_batches)
+  ec = ErrorCollector()
+  model = ResNetModel(n_in=300, n_hidden=n_hidden, n_out=26, n_layer=9)
+  trainer = Trainer(model, train_batches, ec)
+  trainer.train(learning_rate, epochs)
+  ec.plotTrainTestError(model, train_batches.batch_size, learning_rate, epochs)
+  ec.plotTrainTestAcc(model, train_batches.batch_size, learning_rate, epochs)
+  ec.resetErrors()
+  evaluator = Evaluator(model, test_batches, trainer.getSaveFilePath())
+  test_loss, test_acc  = evaluator.eval()
 
