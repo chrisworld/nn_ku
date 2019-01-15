1d4a2d72b0f0576f7c565d9d3e39c902ed32e93d
Alex_A <alexander.albrecht@student.tugraz.at>
2019-01-15 15:03:19 +0100

    Update debug_aa.py

--- task5/debug_aa.py
+++ task5/debug_aa.py
@@ -1,13 +1,13 @@
 from ErrorCollector import ErrorCollector
-from BatchNormalizer import BatchNormalizer
+
 from ClassifiedBatches import ClassifiedBatches
 from Trainer import Trainer
 from Evaluator import Evaluator
-from ResNetModel import ResNetModel
+from ReberBatches import ReberBatches
 from RnnModel import RnnModel
 from RnnModelTester import RnnModelTester
 
-from nn18_ex2_load import load_isolet
+
 
 import numpy as np
 import logging
@@ -17,7 +17,7 @@ import os
 # Main function
 if __name__ == '__main__':
 
-  X, C, X_tst, C_tst = load_isolet()
+
 
   # setup logging 
   log_file_path = os.path.dirname(os.path.abspath( __file__ )) +  os.sep + 'logs' + os.sep
@@ -35,23 +35,20 @@ if __name__ == '__main__':
 
   # Parameters and model
   epochs = 100
-  learning_rate = [1e-3]
-  n_hidden = 40 # number of hidden units within layer
-  n_layer = 1   # number of hidden layers
+  learning_rates = [1e-3]
+  n_hidden = [14] # number of hidden units within layer
+  n_layer = [1]   # number of hidden layers
   activation = ['relu', 'tanh']
-  batch_size = 20
-  max_sequence_length = 1000
+  batch_size = 40
 
-  # Batch Normalizer
-  #bn = BatchNormalizer(X, C, batch_size=batch_size, shuffle=True)
-  #train_batches = bn.getBatches(X, C)
-  #test_batches = bn.getBatches(X_tst, C_tst, test=True)
-  # use Test set as Validation
-  #train_batches.examples_validation = test_batches.examples_validation
-  #train_batches.classes_validation = test_batches.classes_validation
+  n_train_samples, n_val_samples, n_test_samples = 5000, 500, 500
 
-  model_tester = RnnModelTester(epochs, learning_rate, max_sequence_length)
-  model_tester.run(train_batches, test_batches)
+  reber_batches = ReberBatches(n_train_samples, n_val_samples, n_test_samples, batch_size)
+  max_sequence_length = reber_batches.train_max_seq_len
+
+  model_tester = RnnModelTester(epochs, learning_rates, max_sequence_length, n_layer, n_hidden, n_symbols=7,
+               n_out=7, rnn_unit='lstm',)
+  model_tester.run(reber_batches, reber_batches)
 
 
 
